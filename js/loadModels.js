7a6ccf292bfde2e6658b1a2678ed0cb12abf9262
Francisco Estupiñán <festupinan.newrona.net@gmail.com>
2024-06-18 13:14:45 -0500

    Log error and progress models

--- js/loadModels.js
+++ js/loadModels.js
@@ -1,16 +1,19 @@
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import * as THREE from 'three';
 const gltfLoader = new GLTFLoader()
 
 
 let modelo;
-export function loadModel(url) {
-    return new Promise((resolve, reject) => {
+export function loadModel(url, name) {
+    return new Promise((resolve) => {
         gltfLoader.load(url, (gltf) => {
             modelo = gltf;
             resolve(modelo);
-        }, undefined, (error) => {
-            reject(error);
+        },
+        (xhr) => {
+            console.log(`${name} || ${(xhr.loaded / xhr.total * 100)} % loaded`);
+        },
+        (error) => {
+            console.error(`${name} tuvo un error`, error);
         });
     });
 }
